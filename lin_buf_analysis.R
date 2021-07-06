b4d20de91810bc17fcb47dfa74d99bdfa48317e0
vojta-bartak <54854239+vojta-bartak@users.noreply.github.com>
2021-07-06 13:53:46 +0200

    script clean

--- lin_buf_analysis.R
+++ lin_buf_analysis.R
@@ -4,9 +4,8 @@ library(dplyr)
 library(purrr)
 library(nngeo)
 library(tidyr)
-source("lin_buf_library.R")
 
-# input data and parameters
+# input data and parameters ------------------------------------------------------------------
 coast <- read_sf("data/coastline.shp")
 lc <- read_sf("data/Landcover2019_ETRS.shp")
 lcfld <- "Abbr"
@@ -16,21 +15,19 @@ pntid <- "NAME"
 buffer_distance <- 100
 out_file <- paste("indices", buffer_distance, sep="_")
 
-# study areas
+# study areas --------------------------------------------------------------------------------
 bndr300 <- coast %>% 
   st_buffer(300) %>% 
   st_intersection(lc %>% st_union()) %>% 
   st_remove_holes() %>%
   st_union()
-  #clean(thres = 100)
 bndr400 <- coast %>% 
   st_buffer(400) %>% 
   st_intersection(lc %>% st_union()) %>% 
   st_remove_holes() %>%
   st_union() 
-  # clean(thres = 100)
 
-# middle lines
+# middle lines -------------------------------------------------------------------------------
 middle300 <- coast %>%
   st_buffer(150) %>%
   st_union() %>%
@@ -44,7 +41,7 @@ middle400 <- coast %>%
   st_intersection(bndr400) %>%
   st_union()
 
-# visualizing
+# visualizing --------------------------------------------------------------------------------
 tmap_mode("view")
 tm_shape(bndr400) + tm_borders() + tm_fill(col="grey") +
   tm_shape(middle400) + tm_lines(col="red") +
@@ -52,7 +49,22 @@ tm_shape(bndr400) + tm_borders() + tm_fill(col="grey") +
 
 #tm_shape(lc %>% st_union()) + tm_borders() + tm_fill()
 
-# linear buffers
+# function to create a geometry column of linear buffers -------------------------------------
+linear_buffer <- function(pnt, mdln, dist, bndr, width=500){
+  pnt %>% 
+    
+    # snap points
+    st_nearest_points(mdln) %>%
+    st_endpoint() %>%
+    
+    # buffer
+    st_buffer(dist) %>%
+    st_intersection(mdln) %>%
+    st_buffer(width, endCapStyle = "FLAT") %>%
+    st_intersection(bndr %>% st_union())
+}
+
+# linear buffers computation -----------------------------------------------------------------
 pnt <- pnt %>% 
   mutate(
     buffer300 = linear_buffer(pnt, middle300, buffer_distance, bndr300),
@@ -61,7 +73,7 @@ pnt <- pnt %>%
     area400 = st_area(buffer400)
   )
 
-# landcover patches
+# landcover patches --------------------------------------------------------------------------
 patches400 <- lc %>% 
   st_make_valid() %>%
   st_intersection(pnt %>% st_set_geometry("buffer400"))
@@ -71,7 +83,7 @@ patches300 <- lc %>%
 patches400$area <- st_area(patches400)
 patches300$area <- st_area(patches300)
 
-# Class - level indices
+# Class - level indices ----------------------------------------------------------------------
 class_ind300 <- patches300 %>%
   group_by(NAME, Abbr) %>%
   summarize(area = sum(area),
@@ -87,7 +99,7 @@ class_ind400 <- patches400 %>%
             mean_area = sum(area)/length(area)) %>%
   st_drop_geometry()
 
-# Landscape - level indices
+# Landscape - level indices ------------------------------------------------------------------
 land_ind300 <- class_ind300 %>%
   ungroup() %>%
   group_by(NAME) %>%
@@ -101,7 +113,7 @@ land_ind400 <- class_ind400 %>%
             shannon = -sum(rel_area*log(rel_area)),
             simpson = 1 - sum(rel_area*rel_area))
 
-# Adding indices as attributes of input points
+# Adding indices as attributes of input points -----------------------------------------------
 pnt <- pnt %>% 
   left_join(
     class_ind300 %>%
@@ -146,9 +158,12 @@ pnt <- pnt %>%
                 SI400 = simpson
               )) 
 
-# Saving as table
-
-# Saving as shapefile
+# Saving as table ----------------------------------------------------------------------------
 pnt %>%
-  st_write("data/points_with_indices.shp")
+  st_drop_geometry() %>%
+  write.table(paste("data/", out_file, ".csv", sep=""), row.names = F, sep=";")
+
+# Saving as shapefile ------------------------------------------------------------------------
+pnt %>%
+  st_write(paste("data/", out_file, ".shp", sep=""))
 
